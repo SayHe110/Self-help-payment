ca70a78f789b67d1177750ef89be21cba8320564
Leijian <949426374@qq.com>
2018-03-17 07:44:35 +0000

    后台管理-修改文章

--- app/Admin/Controllers/TopicController.php
+++ app/Admin/Controllers/TopicController.php
@@ -3,11 +3,15 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
+use function foo\func;
 
 class TopicController extends Controller
 {
@@ -22,6 +26,13 @@ class TopicController extends Controller
         });
     }
 
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id){
+            $content->header('编辑文章');
+            $content->body($this->form()->edit($id));
+        });
+    }
 
     public function grid()
     {
@@ -35,6 +46,35 @@ class TopicController extends Controller
             $grid->title_image_path('标题图片')->image(60, 60);
             $grid->category()->name('分类');
             $grid->user()->nickname('作者');
+        });
+    }
+
+    public function form()
+    {
+        return Admin::form(Topic::class, function (Form $form){
+            $form->display('id', 'ID');
+            $form->text('title', '标题');
+            $form->select('category_id', '分类')->options(function ($id){
+                $category = Category::find($id);
+
+                if($category)
+                    return [$category->id => $category->name];
+            });
+
+            // todo 富文本编辑器
+            $form->editor('body', '内容');
+            $form->image('title_image_path');
+            $form->select('user_id', '作者')->options(function ($id){
+                $user = User::find($id);
+
+                if($user)
+                    return [$user->id => $user->nickname];
+            });
+            $form->display('view_count', '阅读量');
+            $form->text('excerpt', '摘录');
+            $form->display('created_at', '创建时间');
+            $form->display('updated_at', '修改时间');
+
         });
     }
 
