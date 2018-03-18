3809fef9b9a3169780a3eac9ae13b4472dd97023
Leijian <949426374@qq.com>
2018-03-18 08:10:10 +0000

    修改后台管理菜单填充

--- database/seeds/adminMenuTableSeeder.php
+++ database/seeds/adminMenuTableSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use Encore\Admin\Auth\Database\Menu;
+use Encore\Admin\Auth\Database\Administrator;
+use Encore\Admin\Auth\Database\Role;
+use Encore\Admin\Auth\Database\Permission;
 
 class adminMenuTableSeeder extends Seeder
 {
@@ -12,6 +15,61 @@ class adminMenuTableSeeder extends Seeder
      */
     public function run()
     {
+        // create a user.
+        Administrator::truncate();
+        Administrator::create([
+            'username' => 'admin',
+            'password' => bcrypt('admin'),
+            'name'     => 'Administrator',
+        ]);
+
+        // create a role.
+        Role::truncate();
+        Role::create([
+            'name' => 'Administrator',
+            'slug' => 'administrator',
+        ]);
+
+        // add role to user.
+        Administrator::first()->roles()->save(Role::first());
+
+        //create a permission
+        Permission::truncate();
+        Permission::insert([
+            [
+                'name'        => 'All permission',
+                'slug'        => '*',
+                'http_method' => '',
+                'http_path'   => '*',
+            ],
+            [
+                'name'        => 'Dashboard',
+                'slug'        => 'dashboard',
+                'http_method' => 'GET',
+                'http_path'   => '/',
+            ],
+            [
+                'name'        => 'Login',
+                'slug'        => 'auth.login',
+                'http_method' => '',
+                'http_path'   => "/auth/login\r\n/auth/logout",
+            ],
+            [
+                'name'        => 'User setting',
+                'slug'        => 'auth.setting',
+                'http_method' => 'GET,PUT',
+                'http_path'   => '/auth/setting',
+            ],
+            [
+                'name'        => 'Auth management',
+                'slug'        => 'auth.management',
+                'http_method' => '',
+                'http_path'   => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs",
+            ],
+        ]);
+
+        Role::first()->permissions()->save(Permission::first());
+
         // add default menus.
         Menu::truncate();
         Menu::insert([
