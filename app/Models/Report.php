f1d016a999a7aed319f232ab30be6050be2b27f5
Leijian <949426374@qq.com>
2018-03-18 05:09:34 +0000

    投诉模型及迁移表

---
+++ app/Models/Report.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    protected $fillable = ['user_id', 'title', 'reports', 'description'];
+}
