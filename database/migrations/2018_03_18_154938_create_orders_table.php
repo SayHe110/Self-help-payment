d69632ba9beabbbf390feed1804c947ebfac26dd
Leijian <949426374@qq.com>
2018-03-18 08:10:55 +0000

    订单模型及迁移表

---
+++ database/migrations/2018_03_18_154938_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->integer('order_num')->index()->comment('订单号');
+            $table->integer('dorm_id')->index()->comment('宿舍ID');
+            $table->integer('money');
+            $table->integer('processing_person_id')->nullable()->comment('处理的管理员');
+            $table->boolean('is_handle')->default(false)->comment('是否处理');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
