69a492467f537a6aa63b3f02c9a0c98ebeec020f
SayHe110 <949426374@qq.com>
2018-03-20 19:54:23 +0800

    修改提交订单接口

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\AuthorizationRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Auth;
 
 class AuthorizationsController extends Controller
 {
--- app/Transformers/OrderTransformer.php
+++ app/Transformers/OrderTransformer.php
@@ -20,7 +20,7 @@ class OrderTransformer extends TransformerAbstract
             'id' => $order->id,
             'user_id' => $order->user_id,
             'order_num' => $order->order_num,
-            'dorm_id' => $order->dorm_id,
+            'dormitory_id' => $order->dormitory_id,
             'money' => $order->money,
             'processing_person_id' => $order->processing_person_id,
             'is_handle' => is_bool($order->is_handle),
