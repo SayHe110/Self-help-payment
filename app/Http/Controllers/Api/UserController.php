fc35434addeecefb18f9010877db33153f59a24c
SayHe110 <949426374@qq.com>
2018-03-24 14:44:22 +0800

    用户注册修改

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,26 +10,14 @@ class UserController extends Controller
 {
     public function store(UserRequest $request)
     {
-        /*$captchaData = \Cache::get($request->captcha_key);
-
-        if(!$captchaData){
-            return $this->response->error('验证码已失效',422);
-        }
-        if(! hash_equals($captchaData['captcha'], $request->captcha_code)){
-            // 清除验证码缓存
-            \Cache::forget($request->captcha_key);
-            return $this->response->errorUnauthorized('验证码错误');
-        }*/
-
         $user = User::create([
             'student_id' => $request->student_id,
             'email' => $request->email,
             'password' => bcrypt($request->password),
-            'nickname' => str_random(10),
+            'nickname' => $request->student_id,
+            'avatar' => 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
         ]);
 
-        // 清除验证码缓存
-        // \Cache::forget($request->captcha_key);
         return $this->response->item($user, new UserTransformer())
             ->setMeta([
                 'access_token' => \Auth::guard('api')->fromUser($user),
